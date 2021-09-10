6078886de0b69e2b6b8da78da42b714e9547913b
fsuleman2 <sydsulmn7@gmail.com>
2021-09-11 00:37:09 +0530

    ssew

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -16,7 +16,7 @@ import { TrainformComponent } from './trainform/trainform.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
